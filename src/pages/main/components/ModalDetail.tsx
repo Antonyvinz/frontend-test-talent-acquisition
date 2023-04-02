2770fa552b1498b14d2392690a72b117b52eda3b
antonyvinz <antonyvinz@gmail.com>
2023-04-03 04:39:40 +0700

    just input

--- src/pages/main/components/ModalDetail.tsx
+++ src/pages/main/components/ModalDetail.tsx
@@ -81,6 +81,7 @@ function ModalDetail(props: any) {
                     <Form layout="vertical">
                         <Form.Item label="NAMA LIST ITEM" validateStatus={errors.title && touched.title ? "error" : ""}>
                             <Input
+                                className="modal-add-name-input"
                                 data-cy="modal-add-name-input"
                                 placeholder="Tambahkan nama list item"
                                 value={values.title}
